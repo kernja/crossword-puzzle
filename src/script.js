251c8a50c6e0d0d187c3b40d79eced8a549c3bba
Jeffrey Kern <14278161+kernja@users.noreply.github.com>
2024-10-20 23:48:15 -0400

    update script and add comments

--- src/script.js
+++ src/script.js
@@ -94,7 +94,7 @@ function generateEmptyGameObject()
     }
 
     //goes through many logic checks to ensure if any given space is valid
-    go.isLocationValid = function(x, y, letter, orientation, isFirstLetter, isLastLetter)
+    go.isLocationValid = function(x, y, letter, orientation)
     {
         //horizontal
         if (orientation == 0)
@@ -107,10 +107,6 @@ function generateEmptyGameObject()
                 if (this.isLocationOccupied(x, y + 1)) return false;  
                 if (this.isLocationOccupied(x, y)) return false;  
             }
-
-            if (isFirstLetter && this.isLocationOccupied(x - 1, y)) return false;
-            if (isLastLetter && this.isLocationOccupied(x + 1, y)) return false;
-
         } else {
             //vertical
             if (this.isLocationOutOfBounds(x, y)) return false;
@@ -121,9 +117,6 @@ function generateEmptyGameObject()
                 if (this.isLocationOccupied(x + 1, y)) return false;
                 if (this.isLocationOccupied(x, y)) return false;  
             }
-            if (isFirstLetter && this.isLocationOccupied(x, y - 1)) return false;
-            if (isLastLetter && this.isLocationOccupied(x, y + 1)) return false;
-            
         }
         
         return true;
@@ -176,6 +169,74 @@ function generateEmptyGameObject()
         }    
     }
 
+    //iterate through words and find once that can be placed at a given x/y space and letter
+    go.testWordsAtLocation = function(x, y, letter, orientation)
+    {
+        //filter the list of potential words down to those that contain the letter we're looking for
+        var potentialWords = this.wordPool.filter((x) => x.term.indexOf(letter) >= 0);
+        if (potentialWords.length == 0) return;
+
+        //start the loop
+        var placedWord = false; 
+        var wi = 0;
+        //iterate through all words
+        for (wi = 0; wi < potentialWords.length && placedWord == false; wi++)
+        {
+            //get the word and determine the first index of the matching letter
+            var word = potentialWords[wi];
+            var li = word.term.indexOf(letter);
+            
+            //loop through while there are matches
+            while (li !== -1 && placedWord == false) {
+                // come up with x/y offset
+                // e.g., if the matching letter is the second letter in the word, it will have to
+                // be shifted left one spot or up one spot to fit in the current position
+                var offset = 0 - li;
+                var canPlaceWord = true;
+             
+                //force checks to ensure that a blanks pace exists before and after the word being placed.
+                if (orientation == 0)
+                {
+                    //horizontal
+                    if (this.isLocationValid(x + offset - 1, y,"!", orientation, false, false) == false) canPlaceWord = false;
+                    if (this.isLocationValid(x + offset + word.term.length, y,"!", orientation, false, false) == false) canPlaceWord = false;
+                } else {
+                    //vertical
+                    if (this.isLocationValid(x, y + offset - 1,"!", orientation, false, false) == false) canPlaceWord = false;
+                    if (this.isLocationValid(x, y + offset + word.term.length,"!", orientation, false, false) == false) canPlaceWord = false;
+                }
+
+                //now loop through the letters
+                for (var i = 0; i < word.term.length && canPlaceWord == true; i++)
+                {
+                    if (orientation == 0)
+                    {
+                        if (this.isLocationValid(x + offset + i, y, word.term[i], orientation) == false) canPlaceWord = false;
+                    } else {
+                        if (this.isLocationValid(x, y + offset + i, word.term[i], orientation) == false) canPlaceWord = false;
+                    }
+                }
+
+                // if we made it this far, we can place the word at the offset
+                if (canPlaceWord) {
+                    if (orientation == 0)
+                    {
+                        this.setWordAtLocation(x + offset, y, word, orientation);
+                    } else {
+                        this.setWordAtLocation(x, y + offset, word, orientation);
+                    }
+
+                    placedWord = true;
+                }
+
+                //grab the next instance of the matching letter.
+                //if none are found, -1 is returned and the loop exits.
+                li = word.term.indexOf(letter, li + 1);
+            }
+        }
+
+        return;
+    }
     //go through and validate the puzzle.
     //the tldr; there should always be a blank letter to the left and right of a horizontal word, and
     //there should always be a blank letter to the top and bottom of a vertical word.
@@ -195,76 +256,43 @@ function generateEmptyGameObject()
         return isValidated;
     }
 
-    go.testWordsAtLocation = function(x, y, letter, orientation)
-    {
-        var potentialWords = this.wordPool.filter((x) => x.term.indexOf(letter) >= 0);
-        if (potentialWords.length == 0) return;
-
-        var placedWord = false; 
-        var wi = 0;
-        for (wi = 0; wi < potentialWords.length && placedWord == false; wi++)
-        {
-            var word = potentialWords[wi];
-            var li = word.term.indexOf(letter);
-            
-            while (li !== -1 && placedWord == false) {
-                var offset = 0 - li;
-                var canPlaceWord = true;
-             
-                for (var i = 0; i < word.term.length; i++)
-                {
-                    if (orientation == 0)
-                    {
-                        if (this.isLocationValid(x + offset + i, y, word.term[i], orientation, (i == 0), (i == (word.term[i].length - 1))) == false) canPlaceWord = false;
-                    } else {
-                        if (this.isLocationValid(x, y + offset + i, word.term[i], orientation, (i == 0), (i == (word.term[i].length - 1))) == false) canPlaceWord = false;
-                    }
-                }
-
-                if (canPlaceWord) {
-                    if (orientation == 0)
-                    {
-                        this.setWordAtLocation(x + offset, y, word, orientation);
-                    } else {
-                        this.setWordAtLocation(x, y + offset, word, orientation);
-                    }
-
-                    placedWord = true;
-                }
-
-                li = word.term.indexOf(letter, li + 1);
-            }
-        }
-
-        return;
-    }
-
+    //return the game object
     return go;
 }
 
 function generatePuzzle()
 {
+    //we're gonna loop through and generate a bunch of puzzles
+    //essentially, the more words that can be placed the better.
+    // generate at minimum 25 puzzles to establish the highest number of words that can be likely generated in the word set
+    // then keep generating puzzles until one meets or exceeds that number
     var previousWordCount = 0;
     var currentWordCount = 0;
     var attempts = 0;
 
     do {
+        // refresh our game object
         var go =  generateEmptyGameObject();
 
+        //grab the first word in the pool
         var initialWord = go.wordPool[0];
+        //determine location to set it in the puzzle
         var initialX = Math.floor((go.playfieldSize - initialWord.term.length) * 0.5);
         var initialY = Math.floor((go.playfieldSize - 1) * 0.5);
-
+        //set it in the middle of the puzzle
         go.setWordAtLocation(initialX, initialY, initialWord, 0);
         
-
+        //go through and do loops
         if ((previousWordCount < currentWordCount) && attempts < 25) previousWordCount = currentWordCount;
         currentWordCount = go.placedWords.length;
     
         attempts++;
     } while (currentWordCount < previousWordCount || attempts < 25)
     
+    //give warning if invalid puzzle was generated
     if (go.validatePuzzle() == false) alert("Invalid puzzle was generated. Two words are side-by-side when they shouldn't be.");
+    
+    //render it to screen
     renderPuzzleToScreen(go);
 }
 
